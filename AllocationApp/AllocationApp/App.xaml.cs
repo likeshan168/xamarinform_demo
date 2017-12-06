a84c90e58728815e82cb6fc2a5b3ed07147754f6
likeshan168 <likeshan168@163.com>
2017-12-06 12:10:08 +0800

    remove the appcenter analyzer

--- AllocationApp/AllocationApp/App.xaml.cs
+++ AllocationApp/AllocationApp/App.xaml.cs
@@ -1,8 +1,4 @@
 ﻿using System.Collections.Generic;
-using Microsoft.AppCenter;
-using Microsoft.AppCenter.Analytics;
-using Microsoft.AppCenter.Crashes;
-using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
@@ -22,8 +18,8 @@ namespace AllocationApp
         protected override void OnStart()
         {
             // Handle when your app starts
-            AppCenter.Start("android=70d8deae-b1cb-4e7e-a30a-2890540858b7;",
-                typeof(Analytics), typeof(Crashes));
+            //AppCenter.Start("android=70d8deae-b1cb-4e7e-a30a-2890540858b7;",
+            //    typeof(Analytics), typeof(Crashes));
             //AppCenter.Start("android=70d8deae-b1cb-4e7e-a30a-2890540858b7;" + "uwp={Your UWP App secret here};" +
             //                "ios={Your iOS App secret here}",
             //    typeof(Analytics), typeof(Crashes));
